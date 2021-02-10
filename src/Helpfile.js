4ad51aa7eb50a1b9fb6d0421d3fb08c184775620
MartinG1234 <martin.gidstedt@outlook.com>
2021-02-10 14:35:44 +0100

    Create Helpfile.js

---
+++ src/Helpfile.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+
+//class Car extends React.Component {
+//    constructor(props) {
+//        super(props);
+//    }
+//    render() {
+//        return <h2>I am a {this.props.model}!</h2>;
+//    }
+//}
+
+//ReactDOM.render(<Car model="Mustang" />, document.getElementById('root'));
+
+
+//this.state = { brand: "Ford" };
