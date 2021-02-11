00532a1630c5f8743f23fd3d4857add7cbe864e1
MartinG1234 <martin.gidstedt@outlook.com>
2021-02-11 15:41:29 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 
 let quizData = require('./quiz_data.json')
 
-
-
 class Quiz extends Component {
     constructor(props) {
         super(props);
@@ -11,9 +9,9 @@ class Quiz extends Component {
     }
     render() {
         return (
-            <div><div className="QuizQuestion"> {quizData.quiz_questions[0]
-                .instruction_text}</div></div>
+            <div><div className="QuizQuestion"> {quizData.quiz_questions[0].instruction_text}</div></div>
         )
     }
 }
+
 export default Quiz
