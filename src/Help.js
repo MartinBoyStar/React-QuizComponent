2075eefc29a300fdf025c579af4cf6f6ffeb3d51
MartinG1234 <martin.gidstedt@outlook.com>
2021-02-12 22:36:04 +0100

    Create Help.js

---
+++ src/Help.js
@@ -0,0 +1,11 @@
+//Component är en Class antar jag!?
+
+
+//Nedan är kommentarer i Quiz.js
+//Vad är stat?? samt fs
+//Quiz.js. Importerar från alla de andra filerna. Men vart skickas infon från Quiz.js?
+//Längst ner exporteras ju från denna filen också. Vart då? Till App.js såklart!
+
+
+
+//Är det Classen Quiz som exporteras nedan!? Antar det!! Varför är det alltid Default??
