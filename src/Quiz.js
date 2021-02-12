eaa8c763e5e4b2e4e58466db757d6382a229c890
MartinG1234 <martin.gidstedt@outlook.com>
2021-02-12 22:36:07 +0100

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion.js'
 import QuizEnd from './QuizEnd.js'
 import { stat } from 'fs';
-//Vad är stat?? samt fs
+//Vad är stat?? samt fs. Staistik antar jag, men verkar inte funka eller?
 //Quiz.js. Importerar från alla de andra filerna. Men vart skickas infon från Quiz.js?
 //Längst ner exporteras ju från denna filen också. Vart då? Till App.js såklart!
+
 let quizData = require('./quiz_data.json')
 
 class Quiz extends Component {
@@ -17,11 +18,14 @@ class Quiz extends Component {
             return { quiz_position: state.quiz_position + 1 }
         })
     }
+    handleResetClick() {
+        this.setState({ quiz_position: 1 })
+    }
     render() {
         const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length)
         return (
             <div>
-                {isQuizEnd ? <QuizEnd /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
+                {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> : <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />}
             </div>
         )
     }
